67deaf41cadb490c3d0a0aed68be846c15927090
sergey.trbv <sergey.trbv@yandex.ru>
2023-12-01 21:22:47 +0400

    Убрал лишние заметки

--- src/Main.java
+++ src/Main.java
@@ -14,14 +14,13 @@ import Task.*;
 
 public class Main {
 
-    //Переменные
-    private static final Schedule SCHEDULE = new Schedule();   //Статическая переменная "ГРАФИК"
-    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("d.MM.yyyy");   //Дату оборачиваем в "удобный вид"
-    private static final DateTimeFormatter TIME_FORMAT = DateTimeFormatter.ofPattern("HH:mm");   //Время оборачиваем в "удобный вид"
+    private static final Schedule SCHEDULE = new Schedule();
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("d.MM.yyyy");
+    private static final DateTimeFormatter TIME_FORMAT = DateTimeFormatter.ofPattern("HH:mm");
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);     //объект класса Scanner(Используя методы к этому объекту, сможем вызвать функии ввода в коносль
+        Scanner scanner = new Scanner(System.in);
 
         //Добавляем задачи
         //SCHEDULE.addTask(new SingleTask("SingleTest", "Test", LocalDateTime.now(), TaskType.PERSONAL));
@@ -39,12 +38,12 @@ public class Main {
 
 
     private static void addTask(Scanner scanner) {                                         //МЕТОД "ДОБАВИТЬ ЗАДАЧУ"
-        String title = readString("Введите название задачи: ", scanner);           //Строка title , которая получает readString в котрую передаем cообщение и сканер
-        String discription = readString("Введите описание задачи:", scanner);      //Объект discription , которая получает readString в котрую передаем cообщение и сканер
-        LocalDateTime taskDate = readDateTime(scanner);                                    //Объект taskDate, который получает дату
-        TaskType taskType = readType(scanner);                                             //Объект taskType, котоорый получает тип задачи(Л/Р)
-        Repeatability repeatability = readRepeatability(scanner);                          //Объект repeatability, который получает тип повторяемости
-        Task task = switch (repeatability) {                                               //
+        String title = readString("Введите название задачи: ", scanner);
+        String discription = readString("Введите описание задачи:", scanner);
+        LocalDateTime taskDate = readDateTime(scanner);
+        TaskType taskType = readType(scanner);
+        Repeatability repeatability = readRepeatability(scanner);
+        Task task = switch (repeatability) {
             case SINGLE -> new SingleTask(title, discription, taskDate, taskType);
             case DAILY -> new DailyTask(title, discription, taskDate, taskType);
             case WEEKLY -> new WeeklyTask(title, discription, taskDate, taskType);
@@ -58,19 +57,18 @@ public class Main {
     private static TaskType readType(Scanner scanner) {                                      //МЕТОД "ПРОЧИТАТЬ ТИП ЗАДАЧИ"
         while (true) {
             try {
-                System.out.println("Выберите тип задачи:");                                  //Выводим пользователю список задач
-                for (TaskType taskType : TaskType.values()) {                                //Перебираем все типы задач
-                    System.out.println(taskType.ordinal() + ". " + localizeType(taskType));  /*Выводим по ordinal-значению (по нумерации) + localizeType (Метод приводящий тип задачи)
-                . ordinal() - Возвращает порядковый номер этой константы перечисления, ее позицию в объявлении перечисления, где исходной константе присваивается порядковый номер нуля*/
+                System.out.println("Выберите тип задачи:");
+                for (TaskType taskType : TaskType.values()) {
+                    System.out.println(taskType.ordinal() + ". " + localizeType(taskType));
                 }
                 System.out.print("Введите тип:");
-                String ordinalLine = scanner.nextLine();                                    //Строка ordinalLine принимает значение вводимое пользователем;
-                int ordinal = Integer.parseInt(ordinalLine);                                //Конвертируем строку ordinalLine в целое число int; parseInt преобразует строку в число
-                return TaskType.values()[ordinal];                                          //Возвращаем...
+                String ordinalLine = scanner.nextLine();
+                int ordinal = Integer.parseInt(ordinalLine);
+                return TaskType.values()[ordinal];
             } catch (
-                    NumberFormatException e) {                                             //Обрабатываем исключение если пользователь ввел неправильный идентификатор задачи
+                    NumberFormatException e) {
                 System.out.println("Введён неверный номер типа задачи");
-            } catch (ArrayIndexOutOfBoundsException e) {                                    //Обрабатываем исключение
+            } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Тип задачи не найден");
             }
         }
@@ -79,34 +77,34 @@ public class Main {
     private static Repeatability readRepeatability(Scanner scanner) {                                          //МЕТОД "ПРОЧИТАТЬ ПОВТОРЯЕМОСТЬ ЗАДАЧИ"
         while (true) {
             try {
-                System.out.print("Выберите повторяемость задачи:");                                          //Выводим пользователю список повторяемости задач
-                for (Repeatability repeatability : Repeatability.values()) {                                   //Перебираем все типы повторяемости задач
-                    System.out.println(repeatability.ordinal() + ". " + localizeRepeatability(repeatability)); //Выводим по ordinal-значению (по нумерации) + localizeRepeatability (Метод приводящий тип повторяемости задачи)
+                System.out.print("Выберите повторяемость задачи:");
+                for (Repeatability repeatability : Repeatability.values()) {
+                    System.out.println(repeatability.ordinal() + ". " + localizeRepeatability(repeatability));
                 }
                 System.out.println("Введите повторяемость:");
-                String ordinalLine = scanner.nextLine();                                    //Строка ordinalLine принимает значение вводимое пользователем;
-                int ordinal = Integer.parseInt(ordinalLine);                                //Конвертируем строку ordinalLine в целое число int; parseInt преобразует строку в число
-                return Repeatability.values()[ordinal];                                     //Возвращаем...
+                String ordinalLine = scanner.nextLine();
+                int ordinal = Integer.parseInt(ordinalLine);
+                return Repeatability.values()[ordinal];
             } catch (
-                    NumberFormatException e) {                                             //Обрабатываем исключение если пользователь ввел неправильный идентификатор задачи
+                    NumberFormatException e) {
                 System.out.println("Введён неверный номер типа задачи");
-            } catch (ArrayIndexOutOfBoundsException e) {                                    //Обрабатываем исключение
+            } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Тип задачи не найден");
             }
         }
     }
 
     private static LocalDateTime readDateTime(Scanner scanner) {                 //МЕТОД, СЧИТЫВАЮЩИЙ ВРЕМЯ ЗАДАЧИ
-        LocalDate localDate = readDate(scanner);                                 //Объект localDate ссылается на метод "Прочитать дату" и присваеивает значение пользователя
-        LocalTime localTime = readTime(scanner);                                 //Объект localTime ссылается на метод "Прочитать время" и присваеивает значение пользователя
-        return localDate.atTime(localTime);                                      //Возвращаем объединенные 2 метода с датой и временем
+        LocalDate localDate = readDate(scanner);
+        LocalTime localTime = readTime(scanner);
+        return localDate.atTime(localTime);
 
     }
 
     private static String readString(String message, Scanner scanner) {           //МЕТОД, СЧИТЫВАЮЩИЙ СТРОКУ ПОЛЬЗОВАТЕЛЯ
         while (true) {
             System.out.println(message);
-            String readString = scanner.nextLine();                               //Строке readString присваиваем значение,которое ввёл пользователь
+            String readString = scanner.nextLine();
             if (readString == null || readString.isEmpty()) {
                 System.out.println("Введено пустое значение");
             } else {
@@ -117,42 +115,40 @@ public class Main {
 
     private static void removeTask(Scanner scanner) {                             //МЕТОД "УДАЛИТЬ ЗАДАЧУ"
         System.out.println("Все задачи:");
-        for (Task task : SCHEDULE.getAllTasks()) {                                //Итерируемся по задачам, которые есть в списке
-            System.out.printf("%d. %s [%s](%s)%n",                                //"%идентификатор задачи. %название задачи [%тип задачи Л/Р](%повторяемость)%n"
-                    task.getId(),                                                 //Выводим id задачи
-                    task.getTitle(),                                              //Выводим название задачи
-                    localizeType(task.getTaskType()),                             //Выводим тип (Л/Р) через метод localizeType
-                    localizeRepeatability(task.getRepeatabilityType()));          //Выводим тип повторяемости через метод localizeRepeatability
+        for (Task task : SCHEDULE.getAllTasks()) {
+            System.out.printf("%d. %s [%s](%s)%n",
+                    task.getId(),
+                    task.getTitle(),
+                    localizeType(task.getTaskType()),
+                    localizeRepeatability(task.getRepeatabilityType()));
 
         }
         System.out.println(SCHEDULE.getAllTasks());
         while (true) {
             try {
-                System.out.println("Выберите задачу для удаления:");              //Выводим пользователю список задач
-                String idLine = scanner.nextLine();                               /*Строка idLine принимает значение вводимое пользователем;
-                NextLine () в Java применяется для класса Scanner. Этот метод нужен для получения и чтения данных, написанных пользователем*/
-                int id = Integer.parseInt(idLine);                                //Конвертируем строку idLine в целое число int; parseInt преобразует строку в число
-                SCHEDULE.removeTask(id);                                          //Удаляем задачу
+                System.out.println("Выберите задачу для удаления:");
+                String idLine = scanner.nextLine();
+                int id = Integer.parseInt(idLine);
+                SCHEDULE.removeTask(id);
                 break;
-            } catch (NumberFormatException e) {                                   //Обрабатываем исключение
+            } catch (NumberFormatException e) {
                 System.out.println("Введён неверный id задачи");
-            } catch (TaskNotFoundException e) {                                   //Обрабатываем исключение
+            } catch (TaskNotFoundException e) {
                 System.out.println("Задача для удаления не найдена");
             }
         }
     }
 
-    public static void printTaskForDate(Scanner scanner) {                                //МЕТОД, КОТОРЫЙ ВЫВОДИТ ЗАДАЧИ НА ДЕНЬ (Принимаем сканер)
-        LocalDate localDate = readDate(scanner);                                          //Получаем дату пользователя
-        Collection<Task> taskForDate = SCHEDULE.getTaskForDay(localDate);                 /*Получаем колелкцию задач (getTaskForDay-Метод возвращающий  коллекцию задач...
-                                                                                          ...на сегодняшний день; в него передаем сегодняшний день)*/
+    public static void printTaskForDate(Scanner scanner) {                                //МЕТОД, КОТОРЫЙ ВЫВОДИТ ЗАДАЧИ НА ДЕНЬ
+        LocalDate localDate = readDate(scanner);
+        Collection<Task> taskForDate = SCHEDULE.getTaskForDay(localDate);
         System.out.println("Задачи на " + localDate.format(DATE_FORMAT));
         for (Task task : taskForDate) {
-            System.out.printf("[%s] %s: %s (%s) %n",                                      // "[%тип задачи] %название задачи: %время в которое задача выполняется (%описание задачи) %перенос строки"
-                    localizeType(task.getTaskType()),                                     //Тип задачи
-                    task.getTitle(),                                                      //Название задачи
-                    task.getTaskDateTime().format(TIME_FORMAT),                           //ВРЕМЯ выполнения задачи
-                    task.getDescription());                                               //Описание задачи
+            System.out.printf("[%s] %s: %s (%s) %n",
+                    localizeType(task.getTaskType()),
+                    task.getTitle(),
+                    task.getTaskDateTime().format(TIME_FORMAT),
+                    task.getDescription());
         }
 
     }
@@ -161,11 +157,10 @@ public class Main {
         while (true) {
             try {
                 System.out.println("Введите дату задачи в формате dd.MM.yyyy: ");
-                String dateLine = scanner.nextLine();                                     //Строке dateLine присваиваем значение,которое ввёл пользователь
-                return LocalDate.parse(dateLine, DATE_FORMAT);                            //dateLine преобразовываем с помощью .parse() в "удобный вид" даты
-                //метод parse() получает некоторую строку в качестве входных данных, "извлекает" из нее необходимую информацию и преобразует ее в объект вызывающего класса
+                String dateLine = scanner.nextLine();
+                return LocalDate.parse(dateLine, DATE_FORMAT);
             } catch (
-                    DateTimeException a) {                                                //Ловим ошибку форматирования даты
+                    DateTimeException a) {
                 System.out.println("Введена дата в неверном формате");
             }
         }
@@ -175,16 +170,16 @@ public class Main {
         while (true) {
             try {
                 System.out.println("Введите время задачи в формате hh:mm: ");
-                String dateLine = scanner.nextLine();                                     //Строке dateLine присваиваем значение,которое ввёл пользователь
-                return LocalTime.parse(dateLine, TIME_FORMAT);                            //dateLine преобразовываем с помощью .parse() в "удобный вид" времени
+                String dateLine = scanner.nextLine();
+                return LocalTime.parse(dateLine, TIME_FORMAT);
             } catch (
-                    DateTimeException a) {                                                //Ловим ошибку форматирования даты
+                    DateTimeException a) {
                 System.out.println("Введена дата в неверном формате");
             }
         }
     }
 
-    private static String localizeType(TaskType taskType) { //Метод приводящий тип задачи
+    private static String localizeType(TaskType taskType) {                               //Метод приводящий тип задачи
 
         return switch (taskType) {
             case WORK -> "Рабочая задача";
@@ -192,8 +187,8 @@ public class Main {
         };
     }
 
-    private static String localizeRepeatability(Repeatability repeatability) {             /*Повторяемость(тип) задачи
-        Принимаем класс Repeatability-"Повторяемость задачи"*/
+    private static String localizeRepeatability(Repeatability repeatability) {             //Повторяемость(тип) задачи
+
         return switch (repeatability) {
 
             case SINGLE -> "Разовая";
